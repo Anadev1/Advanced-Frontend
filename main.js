257a7c374ab3a736cb8ca16553b50651a7c9f380
Wojtek <wdzwonek97@gmail.com>
2020-12-04 10:45:30 +0100

    fixed everything

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 // import components, pages and services
+import spaService from "./js/spa.js";
 import NavBar from "./components/nav.js";
 import HomePage from "./pages/home.js";
-import spaService from "./js/spa.js";
+//import Map from "/pages/map.js";
+//import Exhibitions from "./pages/exhbitions.js";
 
-let navBar = new NavBar();
-let homePage = new HomePage();
 
 spaService.init();
+//let map = new Map();
+let nav = new NavBar();
+let homePage = new HomePage();
 
 window.pageChange = () => spaService.pageChange();
--- pages/home.js
+++ pages/home.js
@@ -4,17 +4,19 @@ export default class HomePage{
     }
 
     template(){
-        document.querySelector("#home").innerHTML += /*html*/`
-            <header class="topbar">
-                <h1>KuNSTE</h1>
-                <h2>Art museum</h2>
-            </header>
-            <div class="nav-btn">
-                <div></div>
-                <div></div>
-            </div>
-            <h3>SPA Template Modules Pages</h3>
-            <p>My Single Page Web App Template</p>
+        document.querySelector("#app").innerHTML += /*html*/`
+            <section id="home" class="page home-page">
+                <header class="topbar">
+                    <h1>KuNSTE</h1>
+                    <h2>Art museum</h2>
+                </header>
+                <div class="nav-btn">
+                    <div></div>
+                    <div></div>
+                </div>
+                <h3>SPA Template Modules Pages</h3>
+                <p>My Single Page Web App Template</p>
+            </section>
         `;
     }
 }
