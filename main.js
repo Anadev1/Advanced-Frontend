b53b840373f7fa9286d7959c2d4996926c2e3a56
Maddy <eaammpo@students.eaaa.dk>
2020-12-04 10:27:20 +0100

    Map and firebase fixes

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // import components, pages and services
 import spaService from "./js/spa.js";
+import Map from "/pages/map.js";
 
 spaService.init();
+let map = new Map();
 
 window.pageChange = () => spaService.pageChange();
---
+++ pages/map.js
@@ -0,0 +1,18 @@
+export default class Map{
+    constructor(){
+        this.template();
+    }
+
+    template(){
+        document.querySelector("#app").innerHTML += /*html*/ `
+            
+        <section id="map" class="page">
+
+           <div>
+               <h2>Kunste Museum Map</h2>
+            </div>
+            
+        </section>
+        `;
+    }
+}
