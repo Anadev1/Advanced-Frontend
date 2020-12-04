c481fd2d7fe6a30c0f4242589dd215da901d5c20
sandranielsen <eaasmmn@students.eaaa.dk>
2020-12-04 12:21:54 +0100

    exhibition details styling

--- pages/exhibition-details.js
+++ pages/exhibition-details.js
@@ -5,16 +5,21 @@ export default class ExhibitionDetails {
 
     template() {
         document.querySelector('#app').innerHTML += /*html*/ `
-               <section id="exhibition-details" class="page">
-                  <div id="details_banner">
-                      <img src="media/søndag-morgen.jpg" class="banner_img">
-                      <h3>Human Nature</h3>
+               <section id="exhibition-details" class="page exhibition-details">
+                   <div class="nav-btn">
+                    <div></div>
+                    <div></div>
+                </div>
+                  <div id="details-banner">
+                      <h3 class="exhibition_title">Human Nature</h3>
                   </div>
-                  <div class="details_description">
+                  <div id="details-description">
+                      <div>
                       <h1 class="floor_number">Floor 8</h1>
-                      <div class="description_text">
-                          <h3>Description</h3>
-                          <p>There are certain elements that people, at all times, have 
+                    </div>
+                      <div>
+                          <h3 class="description_title">Description</h3>
+                          <p class="description_text">There are certain elements that people, at all times, have 
                               had to respond to in life: nature, religion, philosophy, and science.
                               KüNSTE has decided to use these themes as common threads in a new presentation 
                               of works from the collection opening in February. The purpose of the exhibition is to 
@@ -22,8 +27,8 @@ export default class ExhibitionDetails {
                               chance to reflect on life. A number of the familiar main works in our collection will, 
                               of course, be included in this new hanging.</p> 
                       </div>
-
                   </div>
+                  <div id="artwork-list"></div>
 
                </section>
           `;
