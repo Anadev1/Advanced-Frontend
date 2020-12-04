e156be60737cee2c0699cd61adc464ae95b2668d
Ana <eaaania@students.eaaa.dk>
2020-12-04 10:44:46 +0100

    init exhibitions

--- js/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// import components, pages and services
-import spaService from "spa.js";
-
-spaService.init();
-
-window.pageChange = () => spaService.pageChange();
--- main.js
+++ main.js
@@ -3,7 +3,9 @@ import spaService from "./js/spa.js";
 import Map from "/pages/map.js";
 import Exhibitions from "./pages/exhbitions.js";
 
+//Declare and init
 spaService.init();
 let map = new Map();
+let exhibitions = new Exhibitions();
 
 window.pageChange = () => spaService.pageChange();
