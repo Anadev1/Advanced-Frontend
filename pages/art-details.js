52cd30f2aade738f698fae70f40a6dc38f887611
Ana <eaaania@students.eaaa.dk>
2020-12-07 14:15:23 +0100

    brain fried

--- pages/art-details.js
+++ pages/art-details.js
@@ -1,37 +1,37 @@
-// import {
-//     firebaseDB
-// } from "../js/firebase_config.js";
+import {
+    firebaseDB
+} from "../js/firebase_config.js";
 
 export default class ArtDetails {
     constructor() {
         this.template();
-     //    this.artworkRef = firebaseDB.collection("artworks");
-     //    this.read();
+        this.artworkRef = firebaseDB.collection("artworks");
+        this.read();
     }
 
-//     read() {
-//         this.artworkRef.onSnapshot(snapshotData => {
-//             let artworks = [];
-//             snapshotData.forEach(doc => {
-//                 let artwork = doc.data();
-//                 artwork.id = doc.id;
-//                 artworks.push(artwork);
-//             });
-//         });
-//      }
+    read() {
+        this.artworkRef.onSnapshot(snapshotData => {
+            let artworks = [];
+            snapshotData.forEach(doc => {
+                let artwork = doc.data();
+                artwork.id = doc.id;
+                artworks.push(artwork);
+            });
+        });
+     }
      
     template() {
         document.querySelector('#app').innerHTML += /*html*/ `
-               <section id="artwork-details" class="page">
+               <section id="art-details" class="page">
                   <div id="details-banner">
                       <h3 class="artwork-title">Boy</h3>
                   </div>
                   <div id="details-container">
-                    <div id="artwork-description">
-                         <h1 class="artist-name"></h1>
-                         <h3 class="artwork-name"></h3>
+                    <div id="artwork-description-left">
+                         <h1 class="artist-name">Ron Mueck</h1>
+                         <h3 class="artwork-name">Boy</h3>
                     </div>
-                    <div>
+                    <div id="artwork-description-right">
                          <h3 class="description-title">Description</h3>
                          <p class="description-text">There are certain elements that people, at all times, have 
                          had to respond to in life: nature, religion, philosophy, and science.
