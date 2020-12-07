7b7a7ba11246f8f64052d7cdfe3bfeed6d44ca6e
Ana <eaaania@students.eaaa.dk>
2020-12-07 13:15:23 +0100

    art details page idk

--- pages/art-details.js
+++ pages/art-details.js
@@ -1,39 +1,39 @@
-import {
-    firebaseDB
-} from "../js/firebase_config.js";
+// import {
+//     firebaseDB
+// } from "../js/firebase_config.js";
 
 export default class ArtDetails {
     constructor() {
         this.template();
-        this.artworkRef = firebaseDB.collection("artworks");
-        this.read();
+     //    this.artworkRef = firebaseDB.collection("artworks");
+     //    this.read();
     }
 
-    read() {
-        this.artworkRef.onSnapshot(snapshotData => {
-            let artworks = [];
-            snapshotData.forEach(doc => {
-                let artwork = doc.data();
-                artwork.id = doc.id;
-                artworks.push(artwork);
-            });
-        });
-     }
+//     read() {
+//         this.artworkRef.onSnapshot(snapshotData => {
+//             let artworks = [];
+//             snapshotData.forEach(doc => {
+//                 let artwork = doc.data();
+//                 artwork.id = doc.id;
+//                 artworks.push(artwork);
+//             });
+//         });
+//      }
      
     template() {
         document.querySelector('#app').innerHTML += /*html*/ `
                <section id="artwork-details" class="page">
                   <div id="details-banner">
-                      <h3 class="art-title">Human Nature</h3>
-                      <img src="/media/map.svg" onclick="navigateTo('map')" alt="map" class="banner_img">
+                      <h3 class="artwork-title">Boy</h3>
                   </div>
-                  <div id="details-description">
-                    <div id="floor-description">
-                         <h1 class="floornumber">Floor 8</h1>
+                  <div id="details-container">
+                    <div id="artwork-description">
+                         <h1 class="artist-name"></h1>
+                         <h3 class="artwork-name"></h3>
                     </div>
                     <div>
-                         <h3 class="description_title">Description</h3>
-                         <p class="description_text">There are certain elements that people, at all times, have 
+                         <h3 class="description-title">Description</h3>
+                         <p class="description-text">There are certain elements that people, at all times, have 
                          had to respond to in life: nature, religion, philosophy, and science.
                          KüNSTE has decided to use these themes as common threads in a new presentation 
                          of works from the collection opening in February. The purpose of the exhibition is to 
@@ -43,8 +43,8 @@ export default class ArtDetails {
                       </div>
                   </div>
                   <div id="artist-artworks">
-                      <h4 class="list_title">Other artworks by this artist:</h4>
-                  <div id="artwork-list"></div>
+                      <h4 class="list-title">Other artworks by this artist:</h4>
+                  <div id="artworks-list"></div>
                 </div>
                </section>
           `;
