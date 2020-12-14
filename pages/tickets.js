c7b295097aeba32dccaa9378326b4ec58ef55ac9
sandranielsen <eaasmmn@students.eaaa.dk>
2020-12-14 13:59:47 +0100

    tickets page adjustments

--- pages/tickets.js
+++ pages/tickets.js
@@ -14,9 +14,11 @@ export default class TicketsPage {
                         <h5 class="ticket_title">Adult<h5> 
                         <div id="ticket-amount">
                                 <h5 class="ticket_price">150DKK<h5> 
-                            <button type="button" value="minus" onclick="updateAmount(this);"><img src="./media/minus.svg" alt="minus" class="minus_btn"></button>
+                        <button type="button" value="minus" onclick="updateAmount(this);"><img src="./media/minus.svg" alt="minus" 
+                        class="minus_btn"></button>
                         <span class="quantity_value" id="quantity">0</span>
-                        <button type="button" value="plus" onclick="updateAmount(this);"><img src="./media/plus.svg" alt="minus" class="plus_btn"></button>
+                        <button type="button" value="plus" onclick="updateAmount(this);"><img src="./media/plus.svg" alt="plus" 
+                        class="plus_btn"></button>
                         </div>    
                         </div>  
 
@@ -29,7 +31,7 @@ export default class TicketsPage {
                             <h5 class="ticket_price">120DKK<h5> 
                         <button type="button" value="minus" onclick="updateAmount1(this);"><img src="./media/minus.svg" alt="minus" class="minus_btn"></button>
                         <span class="quantity_value" id="quantity1">0</span>
-                        <button type="button" value="plus" onclick="updateAmount1(this);"><img src="./media/plus.svg" alt="minus" class="plus_btn"></button>
+                        <button type="button" value="plus" onclick="updateAmount1(this);"><img src="./media/plus.svg" alt="plus" class="plus_btn"></button>
                         </div> 
                         </div>
                         
@@ -39,7 +41,7 @@ export default class TicketsPage {
                             <h5 class="ticket_price">120DKK<h5> 
                         <button type="button" value="minus" onclick="updateAmount2(this);"><img src="./media/minus.svg" alt="minus" class="minus_btn"></button>
                         <span class="quantity_value" id="quantity2">0</span>
-                        <button type="button" value="plus" onclick="updateAmount2(this);"><img src="./media/plus.svg" alt="minus" class="plus_btn"></button>
+                        <button type="button" value="plus" onclick="updateAmount2(this);"><img src="./media/plus.svg" alt="plus" class="plus_btn"></button>
                         </div> 
                         </div>  
 
@@ -53,7 +55,7 @@ export default class TicketsPage {
                             <h5 class="ticket_price">130DKK<h5> 
                         <button type="button" value="minus" onclick="updateAmount3(this);"><img src="./media/minus.svg" alt="minus" class="minus_btn"></button>
                         <span class="quantity_value" id="quantity3">0</span>
-                        <button type="button" value="plus" onclick="updateAmount3(this);"><img src="./media/plus.svg" alt="minus" class="plus_btn"></button>
+                        <button type="button" value="plus" onclick="updateAmount3(this);"><img src="./media/plus.svg" alt="plus" class="plus_btn"></button>
                         </div>
                         </div> 
 
@@ -67,7 +69,7 @@ export default class TicketsPage {
                             <h5 class="ticket_price">FREE<h5> 
                         <button type="button" value="minus" onclick="updateAmount4(this);"><img src="./media/minus.svg" alt="minus" class="minus_btn"></button>
                         <span class="quantity_value" id="quantity4">0</span>
-                        <button type="button" value="plus" onclick="updateAmount4(this);"><img src="./media/plus.svg" alt="minus" class="plus_btn"></button>
+                        <button type="button" value="plus" onclick="updateAmount4(this);"><img src="./media/plus.svg" alt="plus" class="plus_btn"></button>
                         </div> 
                         </div>
 
@@ -85,7 +87,7 @@ export default class TicketsPage {
                             <h5 class="ticket_price">200DKK<h5> 
                         <button type="button" value="minus" onclick="updateAmount5(this);"><img src="./media/minus.svg" alt="minus" class="minus_btn"></button>
                         <span class="quantity_value" id="quantity5">0</span>
-                        <button type="button" value="plus" onclick="updateAmount5(this);"><img src="./media/plus.svg" alt="minus" class="plus_btn"></button>
+                        <button type="button" value="plus" onclick="updateAmount5(this);"><img src="./media/plus.svg" alt="plus" class="plus_btn"></button>
                         </div> 
                         </div>  
 
@@ -96,7 +98,7 @@ export default class TicketsPage {
                                 <form>
                                 <button type="button" value="minus" onclick="updateAmount6(this);"><img src="./media/minus.svg" alt="minus" class="minus_btn"></button>
                                 <span class="quantity_value" id="quantity6">0</span>
-                                <button type="button" value="plus" onclick="updateAmount6(this);"><img src="./media/plus.svg" alt="minus" class="plus_btn"></button>
+                                <button type="button" value="plus" onclick="updateAmount6(this);"><img src="./media/plus.svg" alt="plus" class="plus_btn"></button>
                                 </form>
                         </div> 
                         </div>
@@ -115,9 +117,10 @@ export default class TicketsPage {
           `;
     }
 
+    // increment, decrement and quantity functionality 
     updateAmount(that) {
         let number = document.getElementById('quantity');
-        let num = Math.min(Math.max(parseInt(number.innerHTML), 1), 20);
+        let num = Math.min(Math.max(parseInt(number.innerHTML), 0), 19);
         num = (that.value == "minus") ? --num : ++num;
         number.innerHTML = num;
         console.log("it works");
@@ -125,42 +128,42 @@ export default class TicketsPage {
 
     updateAmount1(that) {
         let number1 = document.getElementById('quantity1');
-        let num1 = Math.min(Math.max(parseInt(number1.innerHTML), 1), 20);
+        let num1 = Math.min(Math.max(parseInt(number1.innerHTML), 0), 19);
         num1 = (that.value == "minus") ? --num1 : ++num1;
         number1.innerHTML = num1;
     }
 
     updateAmount2(that) {
         let number2 = document.getElementById('quantity2');
-        let num2 = Math.min(Math.max(parseInt(number2.innerHTML), 1), 20);
+        let num2 = Math.min(Math.max(parseInt(number2.innerHTML), 0), 19);
         num2 = (that.value == "minus") ? --num2 : ++num2;
         number2.innerHTML = num2;
     }
 
     updateAmount3(that) {
         let number3 = document.getElementById('quantity3');
-        let num3 = Math.min(Math.max(parseInt(number3.innerHTML), 1), 20);
+        let num3 = Math.min(Math.max(parseInt(number3.innerHTML), 0), 19);
         num3 = (that.value == "minus") ? --num3 : ++num3;
         number3.innerHTML = num3;
     }
 
     updateAmount4(that) {
         let number4 = document.getElementById('quantity4');
-        let num4 = Math.min(Math.max(parseInt(number4.innerHTML), 1), 20);
+        let num4 = Math.min(Math.max(parseInt(number4.innerHTML), 0), 19);
         num4 = (that.value == "minus") ? --num4 : ++num4;
         number4.innerHTML = num4;
     }
 
     updateAmount5(that) {
         let number5 = document.getElementById('quantity5');
-        let num5 = Math.min(Math.max(parseInt(number5.innerHTML), 1), 20);
+        let num5 = Math.min(Math.max(parseInt(number5.innerHTML), 0), 19);
         num5 = (that.value == "minus") ? --num5 : ++num5;
         number5.innerHTML = num5;
     }
 
     updateAmount6(that) {
         let number6 = document.getElementById('quantity6');
-        let num6 = Math.min(Math.max(parseInt(number6.innerHTML), 1), 20);
+        let num6 = Math.min(Math.max(parseInt(number6.innerHTML), 0), 19);
         num6 = (that.value == "minus") ? --num6 : ++num6;
         number6.innerHTML = num6;
     }
